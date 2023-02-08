d6e1a8e15995669c3f92cf3eb65663cebb2d4fec
7ankash <psyferblock.io@gmail.com>
2023-02-08 14:31:59 +0200

    first commit

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,5 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import Account from '../components/Account'
 
 const Home = () => {
   const session = useSession()
@@ -11,7 +10,7 @@ const Home = () => {
       {!session ? (
         <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
       ) : (
-        <Account session={session} />
+        <p>Account page will go here.</p>
       )}
     </div>
   )
