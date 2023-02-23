37af3d17885061fba67c2299983febb86d04afe3
claudeMassaad <claude1.massaad@gmail.com>
2023-02-23 19:53:44 +0200

    supaBase auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ function MyApp({
     >
       <NavBar />
       <div className="h-16 mb-0 sm:mb-3"></div>
-      <div className="bg-blue-300 min-h-screen">
+      <div className="bg-gray-300 min-h-screen">
         {/* <div className="bg-gray-200 sm:min-h-screen sm:px-12 py-12 sm:py-8"> */}
         <Component {...pageProps} />
         {/* </div> */}
